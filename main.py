6b1564fae5b9a36a69b7deb1a60c9255a8b2ad3e
lukas1308 <lukas.bock@hotmail.de>
2021-07-14 21:59:02 +0200

    Created main-py file with description

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# Designed by: Lukas Bock
+#-------------
+# Created on: 14.07.2021
+# ------------
+# Version: Python 3.7.6, Spyder 4.0.1
+# -------------
+# Description: 
+# This schript is used to visualise historically the number of accidents per category and 
+# to forecast the values for the future. The method is applied to a data set which contains 
+# the monthly values for traffic accidents in Munich until the end of 2020.
+# ------------
+# Input: 
+#   Input data (csv file) -> (Input/)
+
+# %% Import libraries and functions
+
+# Import libraries
+
