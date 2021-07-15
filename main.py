7d6b1312ac210ab0d143bbf9340f1b9ffc953b23
LukyBo <lukas.bock@hotmail.de>
2021-07-15 16:43:50 +0200

    Generate plot for historically data and changed comments for better understanding of the code

--- main.py
+++ main.py
@@ -17,49 +17,36 @@
 
 # Import libraries
 import pandas as pd
+import MyPlots
 
 # load data
 df = pd.read_csv('Input/Monatszahlen_Verkehrsunfälle.csv')
 
-# Print a list with column names using the pandas.DataFrame.columns method
-list(df.columns)
-
-# Drop unnecessary columns
-df.drop(['VORJAHRESWERT','VERAEND_VORMONAT_PROZENT','VERAEND_VORJAHRESMONAT_PROZENT', 'ZWOELF_MONATE_MITTELWERT'],axis=1 ,inplace=True)
-
-# Drop unnecessary rows
-df = df[df.Month != 'Summe']
-
-# Change Month Column to numbers between 1-12 and safe to df1
-df1 = df['Month'].str.extract('.*(\d{2})', expand = False)
-
-# Replace column 'Month' of df with column of df1
-df = df.assign(Month=df1[:])
-
-# Change the non-numeric objects into integers
-df["Month"] = pd.to_numeric(df["Month"])
-
-# Change the non-string objects into strings to be able to filter the df
-df[['Category', 'Accident-type']] = df[['Category', 'Accident-type']].astype(pd.StringDtype())
-
-# Create Column for days as datetime needs days as input
-df["Day"] = 1
-
-# Obtain a datetime column to be able to visualise historically the number of accidents 
-df['date']=pd.to_datetime(df[['Year', 'Month', 'Day']])
+# Data cleansing and adaption
+df.drop(['VORJAHRESWERT','VERAEND_VORMONAT_PROZENT','VERAEND_VORJAHRESMONAT_PROZENT', 'ZWOELF_MONATE_MITTELWERT'],axis=1 ,inplace=True) # Drop unnecessary columns
+df = df[df.Month != 'Summe'] # Drop unnecessary rows
+df = df.sort_values(by ='Month' , ascending=True) # Sort values by time
+df1 = df['Month'].str.extract('.*(\d{2})', expand = False) # Change Month Column to numbers between 1-12 and safe to df1
+df = df.assign(Month=df1[:]) # Replace column 'Month' of df with column of df1
+df["Month"] = pd.to_numeric(df["Month"]) # Change the non-numeric objects into integers
+df[['Category', 'Accident-type']] = df[['Category', 'Accident-type']].astype(pd.StringDtype()) # Change the non-string objects into strings to be able to filter the df
+df["Day"] = 1 # Create Column for days as datetime needs days as input
+df['date']=pd.to_datetime(df[['Year', 'Month', 'Day']]) # Obtain a datetime column to be able to visualise historically the number of accidents 
 
 # Create df for 'Alkoholunfälle'
-#df_alk = df[df['Category'].isin(['Alkoholunfälle'])]
-                                 #& df['Accident-type'] == 'insgesamt']
+df_alk = df.loc[(df['Category'] == 'Alkoholunf�lle') & (df['Accident-type'] == 'insgesamt')]
 
 # Create df for 'Fluchtunfälle'
+df_flucht = df.loc[(df['Category'] == 'Fluchtunf�lle') & (df['Accident-type'] == 'insgesamt')]
 
 # Create df for 'Verkehrsunfälle'
-#
-MyPlots.historically_data(df)
+df_verkehr = df.loc[(df['Category'] == 'Verkehrsunf�lle') & (df['Accident-type'] == 'insgesamt')]
+
+# Generate plot for the number of accidents per category
+MyPlots.historically_data(df_alk, df_flucht, df_verkehr)
+
+
 
 # Visualise historically the number of accidents per category
 categories = ['Alkoholunfälle', 'Fluchtunfälle', 'Verkehrsunfälle']
 
-for category in categories: 
-    
