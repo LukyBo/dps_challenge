894a3bebe7aa32ff6781ffef7eb66f4828903b95
LukyBo <lukas.bock@hotmail.de>
2021-07-16 19:28:58 +0200

    Adapted column names (that they fit to 'Prophet')

--- MyPlots.py
+++ MyPlots.py
@@ -18,13 +18,13 @@ import matplotlib.pyplot as plt
 
 
 def historically_data(df_alk, df_flucht, df_verkehr):
-    x = df_alk["date"]
+    x = df_alk["ds"]
     fig, ax = plt.subplots()  # Create a figure and an axes.
-    ax.plot(x,df_alk["Value"], label='Alkoholunfälle')  # Plot some data on the axes.
-    ax.plot(x,df_flucht["Value"] , label='Fluchtunfälle')  # Plot more data on the axes...
-    ax.plot(x, df_verkehr["Value"] , label='Verkehrsunfälle')  # ... and some more.
+    ax.plot(x,df_alk["y"], label='Alkoholunfälle')  # Plot some data on the axes.
+    ax.plot(x,df_flucht["y"] , label='Fluchtunfälle')  # Plot more data on the axes...
+    ax.plot(x, df_verkehr["y"] , label='Verkehrsunfälle')  # ... and some more.
     ax.set_xlabel('Time in years')  # Add an x-label to the axes.
     ax.set_ylabel('Number of accidents')  # Add a y-label to the axes.
-    ax.set_title("Historically data of the “Monatszahlen Verkehrsunfälle” Dataset")  # Add a title to the axes.
+    ax.set_title("Historically data accidents")  # Add a title to the axes.
     ax.legend()  # Add a legend.
     plt.savefig("historically_data.svg", format="svg")
